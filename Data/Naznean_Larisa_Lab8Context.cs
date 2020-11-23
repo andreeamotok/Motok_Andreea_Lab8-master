31bd122206507418645f105e15b3035095a64c38
Andreea Motok <andreea.motok@yahoo.com>
2020-11-23 19:41:45 +0200

    Add project files.

---
+++ Data/Naznean_Larisa_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Motok_Andreea_Lab8.Models;
+
+namespace Motok_Andreea_Lab8.Data
+{
+    public class Motok_Andreea_Lab8Context : DbContext
+    {
+        public Motok_Andreea_Lab8Context (DbContextOptions<Motok_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Motok_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Motok_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Motok_Andreea_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Motok_Andreea_Lab8.Data;
+using Motok_Andreea_Lab8.Models;
+
+namespace Motok_Andreea_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Motok_Andreea_Lab8.Data.Motok_Andreea_Lab8Context _context;
+
+        public IndexModel(Motok_Andreea_Lab8.Data.Motok_Andreea_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
